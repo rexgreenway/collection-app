a4954b552f6e9f01eafa067ff3209e5189245743
Rex <rexgreenway@proton.me>
2026-04-01 23:15:29 +0100

    remove file bloat

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,17 +2,49 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"net/http"
 	"time"
 
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
 	"github.com/gin-gonic/gin"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"go.uber.org/zap"
-
 	pb "github.com/rexgreenway/collection-app/internal/gen/collection"
 	"github.com/rexgreenway/collection-app/internal/services/collection"
 	"github.com/rexgreenway/collection-app/internal/storage"
 )
+
+// StartGrpcServer
+func StartGrpcServer(ctx context.Context, logger *zap.SugaredLogger, store storage.Store) error {
+	lis, err := net.Listen("tcp", "localhost:50100")
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	// Register reflection service on gRPC server.
+	reflection.Register(grpcServer)
+
+	// Register CollectionService with the gRPC server
+	pb.RegisterCollectionServiceServer(grpcServer, collection.NewServer(logger, store))
+
+	// Goroutine watches for context cancellation
+	go func() {
+		<-ctx.Done()
+		logger.Info("shutting down grpc server...")
+		grpcServer.GracefulStop() // stops accepting new RPCs, waits for in-flight ones to finish
+	}()
+
+	logger.Info("starting grpc server on :50100")
+
+	return grpcServer.Serve(lis)
+}
 
 // StartHTTPServer ???
 func StartHTTPServer(ctx context.Context, logger *zap.SugaredLogger, store storage.Store) error {
