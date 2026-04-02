ee3b85d82710ca0d41cd72e59899321af95fcd01
Rex <rexgreenway@proton.me>
2026-04-02 17:16:59 +0100

    frontend working with backend

--- web/src/api/fetch.ts
+++ web/src/api/fetch.ts
@@ -8,30 +8,42 @@ import type CollectionClient from "./interface";
 
 export const RestClient: CollectionClient = {
   async listCollections() {
-    const res = await fetch("/collections");
-    return await res.json();
+    const res = await fetch("/v1/collections");
+    if (!res.ok) {
+      throw new Error(`Failed to list collections: ${res.status}`);
+    }
+    return (await res.json()) as ListCollectionsResponse;
   },
   async createCollection(collection: Collection) {
-    const res = await fetch("/collections", {
+    const res = await fetch("/v1/collections", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(collection),
     });
-    return await res.json();
+    if (!res.ok) {
+      throw new Error(`Failed to create collection: ${res.status}`);
+    }
+    return (await res.json()) as Collection;
   },
   async getCollection(id: string) {
-    const res = await fetch(`/collections/${id}`);
-    return await res.json();
+    const res = await fetch(`/v1/collections/${id}`);
+    if (!res.ok) {
+      throw new Error(`Failed to get collections '${id}': ${res.status}`);
+    }
+    return (await res.json()) as GetCollectionResponse;
   },
   async updateCollection(id: string, collection: Collection) {
-    const res = await fetch(`/collections/${id}`, {
+    const res = await fetch(`/v1/collections/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(collection),
     });
-    return await res.json();
+    if (!res.ok) {
+      throw new Error(`Failed to update collection '${id}': ${res.status}`);
+    }
+    return (await res.json()) as Collection;
   },
   async deleteCollection(id: string) {
-    await fetch(`/collections/${id}`, { method: "DELETE" });
+    await fetch(`/v1/collections/${id}`, { method: "DELETE" });
   },
 };
